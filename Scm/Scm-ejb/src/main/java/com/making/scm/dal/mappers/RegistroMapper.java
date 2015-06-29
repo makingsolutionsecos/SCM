906ecb92a3ffd33b1201e59a953ff09328520ee2
Camilo Marroquín <camarroquino@gmail.com>
2015-06-29 16:53:48 -0500

    ADD (Mappers)

---
+++ Scm/Scm-ejb/src/main/java/com/making/scm/dal/mappers/RegistroMapper.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.making.scm.dal.mappers;
+
+import com.making.scm.dto.Dto;
+
+/**
+ *
+ * @author Camilo Marroquin
+ */
+public class RegistroMapper implements IMapper{
+
+    @Override
+    public void entityToDto(Dto dto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void dtoToEntity(Object dto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
