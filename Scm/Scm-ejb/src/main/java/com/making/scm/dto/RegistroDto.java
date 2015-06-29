2d7581e600b2a2b5ae109c7cdd9d3a8ee0394ba6
jorgealfonsobeltran <jorgealfonsobeltran@gmail.com>
2015-06-29 11:49:52 -0500

    MOD Registros y pregunta
    Subidas dtoPregunta

--- Scm/Scm-ejb/src/main/java/com/making/scm/dto/RegistroDto.java
+++ Scm/Scm-ejb/src/main/java/com/making/scm/dto/RegistroDto.java
@@ -1,14 +1,69 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.making.scm.dto;
 
+import java.util.Date;
+
 /**
+ * Clase que representa un registro de sintomas en el sistema
  *
- * @author camilomarroquin
+ * @author jorgealfonsobeltran
  */
 public class RegistroDto {
+    
+    /**
+     * Identificador unico del registro
+     */
+    private int Id;
+    
+    /**
+     * Identificador del usuario que realizá el registro
+     */
+    private int IdUsuario;
+    
+    /**
+     * Fecha en la que se realiza el registro 
+     */
+    private Date fecha;
+
+    /**
+     * @return the Id
+     */
+    public int getId() {
+        return Id;
+    }
+
+    /**
+     * @param Id the Id to set
+     */
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    /**
+     * @return the IdUsuario
+     */
+    public int getIdUsuario() {
+        return IdUsuario;
+    }
+
+    /**
+     * @param IdUsuario the IdUsuario to set
+     */
+    public void setIdUsuario(int IdUsuario) {
+        this.IdUsuario = IdUsuario;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public Date getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
     
 }
