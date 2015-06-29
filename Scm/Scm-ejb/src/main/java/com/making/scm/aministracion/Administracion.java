4d5f6adc1f1b81d63ad23b9c3da3870c23ac2fab
angela <angela.suarez.torres@gmail.com>
2015-06-29 10:11:55 -0500

    ADD
    Creación de proyecto con clases complementarias

---
+++ Scm/Scm-ejb/src/main/java/com/making/scm/aministracion/Administracion.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.making.scm.aministracion;
+
+/**
+ *
+ * @author Jimmy
+ */
+public class Administracion {
+    
+}
