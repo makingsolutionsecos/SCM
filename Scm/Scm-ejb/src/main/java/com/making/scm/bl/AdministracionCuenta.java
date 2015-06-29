f1216852c819936f59ec82a48dcc7ae6c870f57a
makingsolutionsecos <makingsolutionsecos@gmail.com>
2015-06-29 11:14:33 -0500

    Revert "ADD"
    This reverts commit 1f57fd627ca56766821ac711305f149a89cf1a7c.

---
+++ Scm/Scm-ejb/src/main/java/com/making/scm/bl/AdministracionCuenta.java
@@ -0,0 +1,27 @@
+package com.making.scm.bl;
+
+/**
+ * Clase que interactúa con la administración de usuarios del sistema (Doctores y pacientes).
+ * @author Jimmy
+ */
+public class AdministracionCuenta {
+    
+    public int InsertarUsuario()
+    {
+        return 0;
+    }
+//    
+//    public UsuarioDto ObtenerUsuario()
+//    {
+//    }
+   
+//    public List<UsuarioDto> ObtenerUsuarios()
+//    {
+//    }
+    
+//      public int ActualizarUsuario(UsuarioDto)
+//      {
+//          retun 0;
+//      }   
+    
+}
