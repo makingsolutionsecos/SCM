10dadc192632414a6a107f8c4a93a20ac940f1bc
Sergio <sergioforerop@hotmail.com>
2015-07-04 12:56:01 -0500

    add

---
+++ Scm/Scm-ejb/src/main/java/com/making/scm/negocio/AdministracionRegistroRespuesta.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.making.scm.negocio;
+
+import com.making.scm.dal.mappers.RespuestaRegistroMapper;
+import com.making.scm.dto.RegistroRespuestaDto;
+import com.making.scm.fachada.RespuestaRegistroFacade;
+import com.making.scm.persistencia.RespuestaRegistro;
+import javax.ejb.EJB;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AdministracionRegistroRespuesta {
+
+    @EJB
+    private RespuestaRegistroFacade respuestaRegistroFacade;
+
+    public AdministracionRegistroRespuesta() {
+    }
+
+    public void guardarRegistroRespuestaCLiente(RegistroRespuestaDto registroRespuestaDto) {
+
+        RespuestaRegistroMapper respuestaRegistroMapper = new RespuestaRegistroMapper();
+
+        respuestaRegistroFacade.edit((RespuestaRegistro) respuestaRegistroMapper.dtoToEntity(registroRespuestaDto));
+
+    }
+
+}
--- Scm/Scm-ejb/src/main/java/com/making/scm/negocio/AdministracionRegistros.java
+++ Scm/Scm-ejb/src/main/java/com/making/scm/negocio/AdministracionRegistros.java
@@ -5,31 +5,13 @@
  */
 package com.making.scm.negocio;
 
-import com.making.scm.dal.mappers.RespuestaRegistroMapper;
-import com.making.scm.dto.RegistroRespuestaDto;
-import com.making.scm.fachada.RespuestaRegistroFacade;
-import com.making.scm.persistencia.RespuestaRegistro;
-import javax.ejb.EJB;
-
 /**
  *
  * @author jorgealfonsobeltran
  */
 public class AdministracionRegistros {
-    
-    @EJB
-    private RespuestaRegistroFacade respuestaRegistroFacade;
             
     public AdministracionRegistros() {
     }
-    
-    
-   public void guardarRegistroRespuestaCLiente(RegistroRespuestaDto registroRespuestaDto){
-       
-       RespuestaRegistroMapper respuestaRegistroMapper = new RespuestaRegistroMapper();
-       
-       respuestaRegistroFacade.edit((RespuestaRegistro)respuestaRegistroMapper.dtoToEntity(registroRespuestaDto));
-       
-   }
     
 }
