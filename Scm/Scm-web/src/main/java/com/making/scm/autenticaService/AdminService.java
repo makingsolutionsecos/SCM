1a8ef9182915ad73570ce704264deabadbb09ea5
angela <angela.suarez.torres@gmail.com>
2015-07-07 10:10:45 -0500

    ADD (angelaEST) Actualización de action

--- Scm/Scm-web/src/main/java/com/making/scm/autenticaService/AdminService.java
+++ Scm/Scm-web/src/main/java/com/making/scm/autenticaService/AdminService.java
@@ -17,7 +17,7 @@ public class AdminService {
     @GET
     public Response getPageAdmin() {
         int status = 200;
-        String html = "<html lang=\\\"en\\\"><body><form action=\"administrar\" method=\"post\"><center><table><tr><td><br/><br/><br/><br/><br/><br/><br/>"
+        String html = "<html lang=\\\"en\\\"><body><form action=\"autenticaServlet\" method=\"post\"><center><table><tr><td><br/><br/><br/><br/><br/><br/><br/>"
                 + "<h2>Bienvenido Administrador a</h2><br/> <h1><div style=\"color: orange;\">SCM - SISTEMA DE CONTROL DE MIGRA&Ntilde;A</div></h1><br/><br/><br/><br/>"
                 + "<br/><br/></td></tr><table></center><br/>"
                 + "<center><input type=\"submit\" value=\"Continuar\"></center></form></body></html>";
